e2f9a061f136638a347d097766e49e2781d8ec99
Timothy Charette <thepubliccoder@gmail.com>
2018-12-11 18:31:12 -0500

    edits

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,6 +25,7 @@ connection.connect(function (err) {
 function showTable() {
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
+    // console.log(res.RowDataPacket[0].id)
     console.table(res);
     start();
   });
@@ -35,26 +36,69 @@ function start() {
     .prompt([{
         name: "itemID",
         type: "input",
-        message: "What is the product ID of the item you would like to purchase?"
+        message: "What is the product ID of the item you would like to purchase?\n"
       },
       {
         name: "itemQuantity",
         type: "input",
-        message: "How many would you like to purchase?"
+        message: "How many would you like to purchase?\n"
       }
     ])
     .then(function (answer) {
-      // console.log(answer.itemID)
-      // console.log(answer.itemQuantity)
-      // if (answer.itemQuantity < stock_quantity){
-
-      // }
+      // SELECT * FROM products WHERE ?
+      // SELECT stock_quantity FROM products WHERE ?
       connection.query("SELECT * FROM products WHERE ?", {
         id: answer.itemID
       }, function (err, res) {
+        if (answer.itemQuantity > res[0].stock_quantity) {
+          console.log("\nInsufficient quantity! Please select another item.\n")
+          start();
+        } else {
+          var itemID = answer.itemID;
+          var itemQuantity = res[0].stock_quantity - answer.itemQuantity;
+          updateTable(itemID, itemQuantity);
+        }
         if (err) throw err;
-        console.table(res);
-        connection.end();
+        // connection.end();
       });
+    })
+}
+
+function updateTable(itemID, itemQuantity) {
+  // console.log("Updating all" +   "Rocky Road quantities...\n");
+  // var query = connection.query("SELECT * FROM products", function (err, res) {
+  //   console.log("Here is a list of our updated inventory.\n")
+  //   console.table(res)
+  //   console.log(query.sql);
+  //   console.log("itemID: " + itemID);
+  //   console.log("itemQuantity: " + itemQuantity);
+  // });
+  connection.query(
+
+    "UPDATE products SET ? WHERE ?",
+    [{
+        stock_quantity: itemQuantity
+      },
+      {
+        id: itemID
+      }
+    ],
+    function (err, res) {
+      console.log(res.affectedRows + " products updated!\n");
+      inquirer
+        .prompt([{
+          name: "anotherPurchase",
+          type: "confirm",
+          message: "Would you like to amke another purchase?\n"
+        }])
+        .then(function (answer) {
+          if (answer.anotherPurchase) {
+            showTable();
+          }
+        })
+
+      //  connection.end();
+
+
     })
 }
